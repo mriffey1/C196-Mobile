cb22d3bbd900e0c2c62529992a9c4340fc860322
MeganRiffey <mriffe4@wgu.edu>
2022-07-01 19:08:26 -0400

    first commit

---
+++ app/src/main/java/com/example/c196/UI/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.c196.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.c196.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void enterHere(View view) {
+        Intent intent = new Intent(MainActivity.this, TermList.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/c196/UI/TermList.java
@@ -0,0 +1,35 @@
+package com.example.c196.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.c196.R;
+
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public boolean onCreationOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_term_list, menu);
+        return true;
+    }
+
+    public boolean OnOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
