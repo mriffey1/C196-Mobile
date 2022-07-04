c7e0e72d6b9079ecbac8b5bf26d75c4290dde982
MeganRiffey <mriffe4@wgu.edu>
2022-07-04 18:32:19 -0400

    setting up back nav

--- app/src/main/java/com/example/c196/UI/AssessmentsList.java
+++ app/src/main/java/com/example/c196/UI/AssessmentsList.java
@@ -3,6 +3,8 @@ package com.example.c196.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.c196.R;
 
@@ -12,5 +14,21 @@ public class AssessmentsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessments_list);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onCreationOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_term_list, menu);
+        return true;
+    }
+
+    public boolean OnOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
