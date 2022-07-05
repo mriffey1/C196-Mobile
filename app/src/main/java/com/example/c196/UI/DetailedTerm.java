fddeceee222455cf0ecfd14301b80553edeebb8b
MeganRiffey <mriffe4@wgu.edu>
2022-07-05 19:08:25 -0400

    updating and creating detail forms

--- app/src/main/java/com/example/c196/UI/DetailedTerm.java
+++ app/src/main/java/com/example/c196/UI/DetailedTerm.java
@@ -3,14 +3,26 @@ package com.example.c196.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.c196.R;
 
 public class DetailedTerm extends AppCompatActivity {
+    EditText editTitle;
+    EditText editStartDate;
+    EditText editEndDate;
+    String title;
+    int termId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_term);
+        editTitle = findViewById(R.id.editTitle);
+        editStartDate = findViewById(R.id.editStartDate);
+        editEndDate = findViewById(R.id.editEndDate);
+        termId = getIntent().getIntExtra("id", -1);
+        title = getIntent().getStringExtra("title");
+        editTitle.setText(title);
     }
 }
